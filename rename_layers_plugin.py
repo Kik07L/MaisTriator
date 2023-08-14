6052c8de420bffe9df548f66b46f207da1c01349
Kik <62511798+Kik07L@users.noreply.github.com>
2023-08-14 23:36:36 +0200

    beta 2

--- rename_layers_plugin.py
+++ rename_layers_plugin.py
@@ -1,24 +1,67 @@
-import re
-import os
+import torch
+import torchvision.models as models
+import numpy as np
+from PIL import Image
+from torchvision import transforms
 
-# Récupère tous les calques du document actif
-def get_layers():
-    layers = []
-    for i, layer in enumerate(app.activeDocument.layers):
-        layers.append(layer)
-    return layers
+# Charger le modèle ResNet pré-entraîné
+def load_resnet_model():
+    model = models.resnet50(pretrained=True)
+    # Le modèle sera téléchargé automatiquement s'il n'est pas déjà enregistré localement
 
-# Trie les calques par nom en utilisant la fonction sorted() et renomme
-def sort_and_rename_layers(layers):
-    sorted_layers = sorted(layers, key=lambda layer: layer.name)
-    for i, layer in enumerate(sorted_layers):
-        new_name = f"Layer_{chr(65 + i)}"  # Utilise les lettres de l'alphabet (A, B, C, ...)
-        layer.name = new_name
+    # Désactiver l'apprentissage pour les couches existantes
+    for param in model.parameters():
+        param.requires_grad = False
 
-# Fonction principale
+    # Remplacer la dernière couche pour l'adapter au nombre de classes dans tes données
+    num_classes = 3  # Remplace 3 par le nombre de classes que tu as (par exemple, jambes, corps, visage)
+    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+
+    return model
+
+# Fonction pour prédire la classe d'une image
+def predict_class(image, model, class_names):
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    img = transform(image).unsqueeze(0)
+
+    model.eval()
+    with torch.no_grad():
+        outputs = model(img)
+
+    _, predicted = torch.max(outputs, 1)
+    class_idx = predicted.item()
+    class_name = class_names[class_idx]
+    return class_name
+
+# Fonction principale du plugin
 def main():
-    layers = get_layers()
-    sort_and_rename_layers(layers)
+    # Charger le modèle ResNet
+    resnet_model = load_resnet_model()
+    class_names = ["jambes", "corps", "visage"]  # Remplace par les noms de tes classes
+
+    # Récupérer le document actif
+    doc = app.ActiveDocument
+
+    # Parcourir tous les calques du document
+    for layer in doc.Layers:
+        # Vérifier si le calque est visible
+        if not layer.Visible:
+            continue
+
+        # Récupérer l'image du calque
+        image = layer.GetContentAsImage()
+
+        # Prédire la classe du contenu de l'image
+        predicted_class = predict_class(image, resnet_model, class_names)
+
+        # Renommer le calque en utilisant la classe prédite
+        new_name = f"{layer.Name}_{predicted_class}"
+        layer.Name = new_name
 
 if __name__ == "__main__":
     import clr
